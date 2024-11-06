b8baa41c1bbfa7037f6cef002c1eb8c4ded72227
Ahmed1Elsayed <ahmed.m.elsayed20@gmail.com>
2024-11-06 17:50:10 +0200

    finished 2-2-g and 2-2-h

---
+++ CustomerWallet.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW CustomerWallet
+AS
+SELECT W.walletID, W.current_balance, W.currency, W.last_modified_date, W.nationalID, W.mobileNo, C.first_name, C.last_name
+FROM Wallet W INNER JOIN Customer_profile C ON W.nationalID = C.nationalID;
+
+
+
+
+
+
---
+++ E_shopVouchers.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW E_shopVouchers
+AS
+SELECT S.name,V.voucherID,V.value
+FROM (Shop S INNER JOIN Voucher V ON S.shopID = V.shopID) INNER JOIN E_shop E ON S.shopID=E.shopID;
