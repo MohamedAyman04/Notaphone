723dcf53c6f21e91e77b3b0fcb43002fd1d1dc78
yehia4 <yehiawael2004@gmail.com>
2024-11-12 11:41:06 +0200

    2.1 c - 2.2 i,j

---
+++ 2.2/PhysicalStoreVouchers.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW PhysicalStoreVouchers
+AS
+SELECT s.name, v.voucherID, v.value
+FROM Physical_Shop ps
+INNER JOIN Shop s ON ps.shopID = s.shopID
+INNER JOIN Voucher v ON s.shopID = v.shopID
---
+++ 2.2/numberOfCashbackTransactions.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW Num_of_cashback
+AS
+SELECT walletID, count(CashbackID) AS NumberOfCashbackTransactions
+FROM Cashback
+WHERE walletID IS NOT NULL
+GROUP BY walletID;
